6c1e9e26ec7cee123014588dc71f154316969db6
alex <alexander.darlington95@gmail.com>
2022-01-27 20:33:14 -0500

    Initial commit

---
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Alex",
+                        "Xela",
+                        LocalDate.of(1995, Month.OCTOBER, 5),
+                        26
+                )
+        );
+    }
+}
